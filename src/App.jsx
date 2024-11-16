7978283e7213c25ba3cbf7c964b091309f2d0e20
Pengaton <lorddev707@gmail.com>
2024-11-16 12:26:22 +0700

    🐛

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <TonConnectUIProvider manifestUrl="">
+    <TonConnectUIProvider manifestUrl="https://github.com/Pengaton/toncars/blob/main/manifest.json">
       <TonConnectButton />
     </TonConnectUIProvider>
   )
